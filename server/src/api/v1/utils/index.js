897f3e8f4ac04af60f0774f45fe9ace6b0f3b907
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2023-09-02 13:12:33 -0700

    superFlexUtils file bug fixed

--- server/src/api/v1/utils/index.js
+++ server/src/api/v1/utils/index.js
@@ -3,7 +3,7 @@ const csvUtils = require("./csvUtils");
 const errorUtils = require("./errorUtils");
 const fantasyCalcUtils = require("./fantasyCalcUtils");
 const ktcUtils = require("./keepTradeCut/ktcUtils");
-const superFlexUtils = require("./FlexUtils");
+const superFlexUtils = require("./superFlexUtils");
 const ktcDynasty = require("./keepTradeCut/dynastyRankingsUtils");
 const pythonUtils = require("./pythonUtils");
 
