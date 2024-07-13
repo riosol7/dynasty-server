cd5085a5865a6acf7a61a99dd86411a921841633
Pirai Ojeda <87847753+riosol7@users.noreply.github.com>
2024-07-13 01:25:13 -0500

    temp disable superflex

--- server/src/api/v1/routes/playerRoutes.js
+++ server/src/api/v1/routes/playerRoutes.js
@@ -6,7 +6,7 @@ router.get("/", playerController.getPlayers);
 router.get("/ktc", playerController.getKTCDynastyRankings);
 router.get("/ktc/:path", playerController.getKTCPlayerValues);
 router.get("/fantasy_calc", playerController.getFantasyCalcRankings);
-router.get("/superFlex", playerController.getSuperFlexRankings);
+// router.get("/superFlex", playerController.getSuperFlexRankings);
 router.get("/dynastyProcess", playerController.getDynastyProcessRankings);
 router.get("/fantasyPro", playerController.getFantasyPro);
 
--- server/src/api/v1/services/playerService.js
+++ server/src/api/v1/services/playerService.js
@@ -4,7 +4,7 @@ const queryListOfPlayers = async () => {
     try {
         return await playerHelpers.fetchPlayerData();
     } catch (error) {
-        console.error('Error in queryListOfPlayers:', error);
+        console.error('Error in queryListOfPlayers:\n', error);
         throw error;
     }
 }
@@ -13,7 +13,7 @@ const queryListOfKTCPlayerValues = async (path) => {
     try {
         return await playerHelpers.getKTCPlayerValues(path);
     } catch (error) {
-        console.error('Error in queryListOfKTCPlayerValues:', error);
+        console.error('Error in queryListOfKTCPlayerValues:\n', error);
         throw error;
     };
 };
@@ -22,7 +22,7 @@ const queryListOfKTCDynastyRankings = async () => {
     try {
         return await playerHelpers.scrapeListOfKTCDynastyRankings();
     } catch (error) {
-        console.error('Error in queryListOfKTCDynastyRankings:', error);
+        console.error('Error in queryListOfKTCDynastyRankings:\n', error);
         throw error;
     }
 };
@@ -31,7 +31,7 @@ const queryListOfFantasyCalcRankings = async () => {
     try {
         return await playerHelpers.scrapeListOfFantasyCalcRankings();
     } catch (error) {
-        console.error('Error in queryListOfFantasyCalcRankings:', error);
+        console.error('Error in queryListOfFantasyCalcRankings:\n', error);
         throw error;
     }
 }
@@ -40,7 +40,7 @@ const queryListOfSuperFlexRankings = async () => {
     try {
         return await playerHelpers.scrapeListOfSuperFlexRankings();
     } catch (error) {
-        console.error('Error in queryListOfSuperFlexRankings:', error);
+        console.error('Error in queryListOfSuperFlexRankings:\n', error);
         throw error;
     }
 }
@@ -49,7 +49,7 @@ const queryListOfDynastyProcessRankings = async () => {
     try {
         return await playerHelpers.scrapeListOfDynastyProcessRankings();
     } catch (error) {
-        console.error('Error in queryListOfDynastyProcessRankings:', error);
+        console.error('Error in queryListOfDynastyProcessRankings:\n', error);
         throw error;
     }
 }
@@ -58,7 +58,7 @@ const queryListOfFantasyPro = async () => {
     try {
         return await playerHelpers.scrapeListOfFantasyPro();
     } catch (error) {
-        console.error('Error in queryListOfFantasyPro:', error);
+        console.error('Error in queryListOfFantasyPro:\n', error);
         throw error;
     }
 }
